0d542a6f7c8c375c44e5fac7cecd624a9504d434
Anton Chaus <anchaus@c1r2p1.khpi.ucode-connect.study>
2020-12-08 15:06:57 +0200

    add sort

--- src/main.c
+++ src/main.c
@@ -231,6 +231,30 @@ void print_node(t_node *node) {
     mx_printstr("\n========================================\n");
 }
 
+void sort_paths(t_node **paths) {
+    for(int i = 0; paths[i + 1] != NULL; i++) {
+        char **paths1 = first_last_name(paths[i]);
+        char **paths2 = first_last_name(paths[i + 1]);
+        if(mx_strcmp(paths1[1], paths2[1]) == 0 
+        && mx_strcmp(paths1[0], paths2[0]) == 0) {
+            t_node *node_path1 = paths[i];
+            t_node *node_path2 = paths[i + 1];
+            for(; node_path1->parent != NULL && node_path2->parent != NULL;) {
+                if(node_path1->to_parent > node_path2->to_parent) {
+                    t_node *temp = paths[i];
+                    paths[i] = paths[i + 1];
+                    paths[i + 1] = temp;
+                    break;
+                }
+                else {
+                    node_path1 = node_path1->parent;
+                    node_path2 = node_path2->parent;
+                }
+            }
+        }
+    }
+}
+
 
 void clean_and_output(t_node **res, int size) {
     t_node **res_temp  = (t_node **) malloc(size * sizeof(t_node *));
@@ -268,6 +292,8 @@ void clean_and_output(t_node **res, int size) {
         }
         printf("\n\n");
     }*/
+
+    sort_paths(res_temp);
     for(int i = 0; res_temp[i] != NULL; i++) {
         print_node(res_temp[i]);
     }
